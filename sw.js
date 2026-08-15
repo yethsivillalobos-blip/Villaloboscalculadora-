b57be317b5bf6332d2e5f54aa808efa7a57b0341
yethsivillalobos-blip <yethsivillalobos@gmail.com>
2026-08-15 02:20:33 -0400

    Add service worker for resource caching
    Implement service worker for caching resources.

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_CALC = 'calculadora-textil-v1';
+
+const recursosCalc = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icons/wolf-icon-192x192.png',
+  './assets/icons/wolf-icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_CALC).then(cache => cache.addAll(recursosCalc))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_CALC) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
